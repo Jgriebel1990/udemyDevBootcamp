1fd4d64b26c2817260818ee81f896c4052a2e7c7
Jefferson Griebel <jeffersonc.griebel@gmail.xom>
2020-09-16 18:53:00 -0400

    created a little JS example

---
+++ section10/app.js
@@ -0,0 +1,8 @@
+const firstName = prompt('What is your first name?');
+const lastName = prompt('What is your last name?');
+const age = prompt('What is your age?');
+const fullName = 'Your full name is ' + firstName + ' ' + lastName;
+const ageAnswer = 'You are ' + age + ' years old';
+
+console.log(fullName);
+console.log(ageAnswer);
